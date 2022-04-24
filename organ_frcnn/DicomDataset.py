defe8d3d0a82be1465f9c17ed7659cbd8d10f76d
hillce <charles.hill@dtc.ox.ac.uk>
2022-04-24 20:17:10 +0100

    dataset documented

--- organ_frcnn/DicomDataset.py
+++ organ_frcnn/DicomDataset.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import os, csv, copy
+import os
 
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -11,29 +11,37 @@ import pydicom
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from skimage import io, transform
+from skimage import transform
+
+from config import classDict, imgMean, imgStd
 
 
 class DicomDataset(Dataset):
     """ Dicom Dataset."""
 
-    def __init__(self,csvFile,rootDir,transform=None,maxRegions=10):
+    def __init__(
+        self,
+        csvFile: str,
+        rootDir: str,
+        transform=None,
+        maxRegions=10,
+        img_dtype=np.float16):
         """
         Args:
             csvFile (str): Path to data.csv with name and x,y coordinates
-            rootDir (str): Directory to all the dicoms
+            rootDir (str): Path to directory for all the dicoms
             transform (callable, optional): Optional transform to be applied on a sample.
+            maxRegions (): ...
+            img_dtype (type): the dtype to be applied to the image.
+
         """
         self.data = pd.read_csv(csvFile,header=None)
 
         self.rootDir = rootDir
         self.transform = transform
-
-        self.classDict = {'Bg':0,'Body':1,'Liver':2,'Cyst':3,'Lung':4,'Heart':5,
-                            'Bg ':0,'Body ':1,'Liver ':2,'Cyst ':3,'Lung ':4,'Heart ':5}
-        
-        self.classDict = {'Bg':0,'Body':1,'Liver':2,'Cyst':3,'Lung':4,'Heart':5,'Spleen':6,'Aorta':7,'Kidney':8,'IVC':9,
-                            'Bg ':0,'Body ':1,'Liver ':2,'Cyst ':3,'Lung ':4,'Heart ':5,'Spleen ':6,'Aorta ':7,'Kidney ':8,'IVC ':9}
+        self.classDict = classDict
+        self.maxRegions = maxRegions
+        self.img_dtype = img_dtype
 
     def __len__(self):
         return len(self.data)
@@ -42,10 +50,9 @@ class DicomDataset(Dataset):
         dcmName = os.path.join(self.rootDir,self.data.iloc[idx,0])
         dcmList = [os.path.join(dcmName,x) for x in os.listdir(dcmName) if x.endswith('.dcm')]
 
-        instTime = {}
+        instTime = {} # Sort the images by instance time
         for dicom in dcmList:
             ds = pydicom.dcmread(dicom,stop_before_pixels=True)
-            #print(ds)
             if 'M' in ds.ImageType:
                 instTime[float(ds.InstanceCreationTime)] = dicom
 
@@ -62,34 +69,18 @@ class DicomDataset(Dataset):
         ds = pydicom.dcmread(instTime[keys[6]])
         img[:,:,2] = ds.pixel_array
         
-        #img = self.resize_img(img,img0.shape[0],img0.shape[1])
+        img = img[:,:,(2,1,0)].astype(self.img_dtype)
 
-        img = img[:,:,(2,1,0)].astype(np.int)
-
-        #x0 = self.data.iloc[idx,0]
-        #y0 = self.data.iloc[idx,1]
-        #w = self.data.iloc[idx,2]
-        #h = self.data.iloc[idx,3]
-        #clsNm = self.data.iloc[idx,4]
 
         labels = self.data.iloc[idx,1:]
         labels = np.array([labels])
         labels = labels.reshape(-1,5)
 
         for i in range(len(labels)):
-            # if "IVC" in self.classDict.keys():
-            #     labelsCopy = copy.deepcopy(labels[i,:])
-            #     labelsCopy[:4] = labels[i,1:]
-            #     labelsCopy[4] = labels[i,0]
-            #     labels[i,:] = labelsCopy
-
             if pd.isna(labels[i,4]):
                 break
 
-
             labels[i,4] = self.classDict[labels[i,4]]
-
-
 
         sample = {'image':img,'labels':labels}
         
@@ -98,7 +89,18 @@ class DicomDataset(Dataset):
         
         return sample
 
-    def resize_img(self,img,width,height,resizeSize=400):
+    def resize_img(self, img, width, height, resizeSize=400):
+        """Function to resize the image.
+
+        Args:
+            img (np.ndarray): image to resize
+            width (int): target width
+            height (int): target height
+            resizeSize (int, optional): maximal length. Defaults to 400.
+
+        Returns:
+            newImg (np.ndarray): resized image
+        """
         if width <= height:
             f = float(resizeSize) / width
             resized_height = int(f * height)
@@ -108,7 +110,7 @@ class DicomDataset(Dataset):
             resized_width = int(f * width)
             resized_height = int(resizeSize)
 
-        newImg = cv2.resize(img,(resized_height,resized_width),interpolation=cv2.INTER_CUBIC)
+        newImg = cv2.resize(img, (resized_height, resized_width), interpolation=cv2.INTER_CUBIC)
         return newImg
 
 class Rescale(object):
@@ -137,13 +139,15 @@ class Rescale(object):
             new_h, new_w = self.output_size
 
         new_h, new_w = int(new_h), int(new_w)
-        img = transform.resize(image,(new_h,new_w),preserve_range=True)
-        for i in range(img.shape[2]):
-            img[:,:,i] = 255*img[:,:,i]//np.amax(img[:,:,i])
-        img = img.astype(np.uint8)
-        #img = cv2.resize(image,(new_h,new_w),interpolation=cv2.INTER_CUBIC)
-        # h and w are swapped for landmarks because for images,
-        # x and y axes are axis 1 and 0 respectively
+        img = transform.resize(image, (new_h, new_w), preserve_range=True)
+
+        # for i in range(img.shape[2]):
+        #     img[:,:,i] = 255*img[:,:,i]//np.amax(img[:,:,i])
+        # img = img.astype(np.uint8)
+        # #img = cv2.resize(image,(new_h,new_w),interpolation=cv2.INTER_CUBIC)
+        # # h and w are swapped for landmarks because for images,
+        # # x and y axes are axis 1 and 0 respectively
+        
         newLabels = []
         for label in labels:
             if np.isnan(label[0]):
@@ -161,7 +165,7 @@ class RandomCrop(object):
 
     Args:
         output_size (tuple or int): Desired output size. If int, square crop
-            is made.
+            is made.    
     """
 
     def __init__(self, output_size):
@@ -197,16 +201,16 @@ class RandomCrop(object):
 class ToTensor(object):
     """ convert ndarrays in sample to Tensors"""
 
-    def __call__(self,sample):
+    def __call__(self, sample):
         image, labels = sample['image'], sample['labels']
         image = image.transpose((2,0,1))
         return {'image': torch.from_numpy(image),'labels': torch.from_numpy(labels)}
 
 class Normalise(object):
+    """Normalise input images with mean and std"""
 
     def __init__(self):
-
-        self.normImg = transforms.Normalize([23.15050654, 51.43708248, 17.58093937],[33.52329497, 84.62633451, 29.45570738],inplace=True)
+        self.normImg = transforms.Normalize(imgMean, imgStd, inplace=True)
 
     def __call__(self,sample):
         img = sample['image'].float()
